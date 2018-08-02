301ae5c6266fc89f3b6bd9e72830452443a5dae9
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-02 14:22:47 -0400

    Rename the FirstViewController to HomeViewController

--- RickyBooks/HomeViewController.swift
+++ RickyBooks/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  HomeViewController.swift
 //  RickyBooks
 //
 //  Created by Ricky Dam on 2018-08-01.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
