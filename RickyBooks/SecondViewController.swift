3b2d6aee304d48d3c62f87a3baebde44086c2db1
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-01 20:18:57 -0400

    Initialize the project

---
+++ RickyBooks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RickyBooks
+//
+//  Created by Ricky Dam on 2018-08-01.
+//  Copyright © 2018 RickyBooks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
