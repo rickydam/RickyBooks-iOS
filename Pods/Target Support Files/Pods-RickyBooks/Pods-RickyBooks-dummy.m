e708e98b95fa5aaf8b452c218a53e930ddb16aae
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-06 18:11:42 -0400

    Add Cocoapods to the project

---
+++ Pods/Target Support Files/Pods-RickyBooks/Pods-RickyBooks-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RickyBooks : NSObject
+@end
+@implementation PodsDummy_Pods_RickyBooks
+@end
---
+++ Pods/Target Support Files/Pods-RickyBooksUITests/Pods-RickyBooksUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RickyBooksUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RickyBooksUITests
+@end
