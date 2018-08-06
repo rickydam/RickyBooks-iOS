20e876c8187c34b58bd3b9dea76b3269f9ef1f13
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-06 18:21:47 -0400

    Edit BuyTableView to fetch and load data into the TextbookCell

--- RickyBooks/BuyTableViewController.swift
+++ RickyBooks/BuyTableViewController.swift
@@ -7,17 +7,76 @@
 //
 
 import UIKit
+import Kingfisher
+
+struct Textbook: Decodable {
+    var id: Int
+    var textbook_title: String
+    var textbook_author: String
+    var textbook_edition: String
+    var textbook_condition: String
+    var textbook_type: String
+    var textbook_coursecode: String
+    var textbook_price: String
+    var created_at: String
+    var user_id: Int
+    var user: User
+    var images: [Image]
+}
+
+struct User: Decodable {
+    var name: String
+}
+
+struct Image: Decodable {
+    var url: String
+}
+
+class TextbookTableViewCell: UITableViewCell {
+    @IBOutlet weak var textbookTitleLabel: UILabel!
+    @IBOutlet weak var textbookAuthorLabel: UILabel!
+    @IBOutlet weak var textbookEditionLabel: UILabel!
+    @IBOutlet weak var textbookConditionLabel: UILabel!
+    @IBOutlet weak var textbookTypeLabel: UILabel!
+    @IBOutlet weak var textbookCoursecodeLabel: UILabel!
+    @IBOutlet weak var textbookPriceLabel: UILabel!
+    @IBOutlet weak var textbookTimestampLabel: UILabel!
+    @IBOutlet weak var textbookSellerLabel: UILabel!
+    @IBOutlet weak var textbookImageView: UIImageView!
+}
 
 class BuyTableViewController: UITableViewController {
-
+    private var textbooks = [Textbook]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(getTextbooks(_:)), for: .valueChanged)
+        self.refreshControl = refreshControl
+        getTextbooks((Any).self)
+    }
+    
+    @objc private func getTextbooks(_ sender: Any) {
+        let urlString = "https://rickybooks.herokuapp.com/textbooks"
+        guard let url = URL(string: urlString) else {
+            print("Error creating the URL")
+            return
+        }
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else {
+                print("Error with the data received")
+                return
+            }
+            do {
+                self.textbooks = try JSONDecoder().decode([Textbook].self, from: data)
+            } catch let jsonError {
+                print("Error with JSONDecoder", jsonError)
+            }
+            DispatchQueue.main.sync {
+                self.tableView.reloadData()
+                self.refreshControl?.endRefreshing()
+            }
+        }.resume()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,68 +87,33 @@ class BuyTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return textbooks.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TextbookCell", for: indexPath) as! TextbookTableViewCell
+        
+        let textbook = textbooks[indexPath.row]
+        cell.textbookTitleLabel?.text = textbook.textbook_title
+        cell.textbookAuthorLabel?.text = textbook.textbook_author
+        cell.textbookEditionLabel?.text = textbook.textbook_edition
+        cell.textbookConditionLabel?.text = textbook.textbook_condition
+        cell.textbookTypeLabel?.text = textbook.textbook_type
+        cell.textbookCoursecodeLabel?.text = textbook.textbook_coursecode
+        cell.textbookPriceLabel?.text = "$ " + textbook.textbook_price
+        cell.textbookTimestampLabel?.text = textbook.created_at
+        cell.textbookSellerLabel?.text = textbook.user.name
+        if textbook.images.count > 0 {
+            let url = URL(string: textbook.images[0].url)
+            cell.textbookImageView?.kf.setImage(with: url)
+        }
+        else {
+            cell.textbookImageView?.kf.base.image = UIImage(named: "TextbookPlaceholder")
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
