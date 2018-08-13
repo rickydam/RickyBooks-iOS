e5f2615bb1bcbefc207423ba65f395e8323a0f84
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-13 15:36:54 -0400

    Implement the AuthorizeView's Register and Login

--- RickyBooks/AuthorizeViewController.swift
+++ RickyBooks/AuthorizeViewController.swift
@@ -7,6 +7,20 @@
 //
 
 import UIKit
+import KeychainAccess
+
+struct RegisterErrors: Decodable {
+    var name: Array<String>?
+    var email: Array<String>?
+    var password: Array<String>?
+    var password_confirmation: Array<String>?
+}
+
+struct UserData: Decodable {
+    var token: String
+    var user_id: Int
+    var name: String
+}
 
 class AuthorizeViewController: UIViewController {
     @IBOutlet weak var registerLoginSegment: UISegmentedControl!
@@ -37,6 +51,178 @@ class AuthorizeViewController: UIViewController {
         }
     }
     
+    @IBAction func registerSubmitPressed(_ sender: Any) {
+        let nameInput = registerNameField.text!
+        let emailInput = registerEmailField.text!
+        let passwordInput = registerPasswordField.text!
+        let confirmPasswordInput = registerConfirmPasswordField.text!
+
+        let endpoint = "https://rickybooks.herokuapp.com/users"
+        var request = URLRequest(url: URL(string: endpoint)!)
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        let registerDictionary: [String: Any] = [
+            "user": [
+                "name": nameInput,
+                "email": emailInput,
+                "password": passwordInput,
+                "password_confirmation": confirmPasswordInput
+            ]
+        ]
+        guard let userObj = try? JSONSerialization.data(withJSONObject: registerDictionary, options: []) else {
+            print("Error with userObj JSONSerialization")
+            return
+        }
+        request.httpBody = userObj
+
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data else {
+                print("Error with the data received")
+                return
+            }
+            do {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode
+                if(statusCode == 200) {
+                    let userData = try JSONDecoder().decode(UserData.self, from: data)
+                    DispatchQueue.main.async {
+                        self.successfulAuthorization(userData: userData)
+                    }
+                }
+                else if(statusCode == 422) {
+                    let errors = try JSONDecoder().decode(RegisterErrors.self, from: data)
+                    var errorMessage = ""
+
+                    let nameError = errors.name
+                    if(nameError != nil) {
+                        if(nameError![0] == "can't be blank") {
+                            if(errorMessage != "") {
+                                errorMessage += "\n"
+                            }
+                            errorMessage += "Missing: Name"
+                        }
+                    }
+                    let emailError = errors.email
+                    if(emailError != nil) {
+                        for entry in emailError! {
+                            if(entry == "can't be blank") {
+                                if(errorMessage != "") {
+                                    errorMessage += "\n"
+                                }
+                                errorMessage += "Missing: Email"
+                            }
+                            if(entry == "is invalid") {
+                                if(errorMessage != "") {
+                                    errorMessage += "\n"
+                                }
+                                errorMessage += "Invalid: Email"
+                            }
+                            if(entry == "has already been taken") {
+                                if(errorMessage != "") {
+                                    errorMessage += "\n"
+                                }
+                                errorMessage += "Taken: Email"
+                            }
+                        }
+                    }
+                    let passwordError = errors.password
+                    if(passwordError != nil) {
+                        if(passwordError![0] == "can't be blank") {
+                            if(errorMessage != "") {
+                                errorMessage += "\n"
+                            }
+                            errorMessage += "Missing: Password"
+                        }
+                    }
+                    let passwordConfirmationError = errors.password_confirmation
+                    if(passwordConfirmationError != nil) {
+                        if(passwordConfirmationError![0] == "doesn't match Password") {
+                            if(errorMessage != "") {
+                                errorMessage += "\n"
+                            }
+                            errorMessage += "Mismatch: Password"
+                        }
+                    }
+
+                    let alert = UIAlertController(title: "Uh oh... we got problems!", message: errorMessage, preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
+                        action in switch action.style {
+                        case .default:
+                            print("Default")
+                        case .cancel:
+                            print("Cancel")
+                        case .destructive:
+                            print("Destructive")
+                        }
+                    }))
+                    DispatchQueue.main.async {
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+                else {
+                    print("Error with the response.")
+                }
+            } catch let jsonError {
+                print("Error with JSONDdecoder", jsonError)
+            }
+        }.resume()
+    }
+    
+    @IBAction func loginSubmitPressed(_ sender: Any) {
+        let emailInput = loginEmailField.text!
+        let passwordInput = loginPasswordField.text!
+
+        let endpoint = "https://rickybooks.herokuapp.com/login"
+        var request = URLRequest(url: URL(string: endpoint)!)
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        let loginDictionary: [String: Any] = [
+            "email": emailInput,
+            "password": passwordInput
+        ]
+        guard let userObj = try? JSONSerialization.data(withJSONObject: loginDictionary, options: []) else {
+            print("Error with userObj JSONSerialization")
+            return
+        }
+        request.httpBody = userObj
+
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data else {
+                print("Error with the data received")
+                return
+            }
+            do {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode
+                if(statusCode == 200) {
+                    let userData = try JSONDecoder().decode(UserData.self, from: data)
+                    DispatchQueue.main.async {
+                        self.successfulAuthorization(userData: userData)
+                    }
+                }
+                else if(statusCode == 401) {
+                    let alert = UIAlertController(title: "Typo! Try again!", message: "Invalid email/password combination", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
+                        action in switch action.style {
+                        case .default:
+                            print("Default")
+                        case .cancel:
+                            print("Cancel")
+                        case .destructive:
+                            print("Destructive")
+                        }
+                    }))
+                    DispatchQueue.main.async {
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+                else {
+                    print("Error with the response.")
+                }
+            } catch let jsonError {
+                print("Error with JSONDecoder", jsonError)
+            }
+        }.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -62,7 +248,31 @@ class AuthorizeViewController: UIViewController {
         registerSubmitButton.layer.cornerRadius = 5
         loginSubmitButton.layer.cornerRadius = 5
     }
-
+    
+    func successfulAuthorization(userData: UserData) {
+        let keychain = Keychain(service: "com.rickybooks.rickybooks")
+        keychain["token"] = userData.token
+        keychain["user_id"] = String(userData.user_id)
+        keychain["name"] = userData.name
+        
+        var viewControllers = self.tabBarController?.viewControllers
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let messagesVC = storyboard.instantiateViewController(withIdentifier: "Messages")
+        messagesVC.tabBarItem = UITabBarItem(title: "Messages", image: UIImage(named: "Messages"), tag: 2)
+        viewControllers![2] = messagesVC
+        
+        let sellVC = storyboard.instantiateViewController(withIdentifier: "Sell")
+        sellVC.tabBarItem = UITabBarItem(title: "Sell", image: UIImage(named: "Sell"), tag: 3)
+        viewControllers![3] = sellVC
+        
+        let profileVC = storyboard.instantiateViewController(withIdentifier: "Profile")
+        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(named: "Profile"), tag: 4)
+        viewControllers![4] = profileVC
+        
+        self.tabBarController?.setViewControllers(viewControllers, animated: false)
+    }
+    
     @objc private func dismissKeyboard(_ sender: Any) {
         view.endEditing(true)
     }
