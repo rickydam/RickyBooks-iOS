b07c1e49c45e8d31c92c2e876ca134bab0ebe201
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-17 21:39:51 -0400

    Create the DetailsViewController in Swift

---
+++ RickyBooks/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  RickyBooks
+//
+//  Created by Ricky Dam on 2018-08-17.
+//  Copyright © 2018 RickyBooks. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
