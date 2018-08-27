0cd57c20c4cb227c1cd7e6eae8972ca1f9ee5eab
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-27 18:18:31 -0400

    Remove successfulAuthorization func from AuthorizeVC

--- RickyBooks/AuthorizeViewController.swift
+++ RickyBooks/AuthorizeViewController.swift
@@ -243,30 +243,6 @@ class AuthorizeViewController: UIViewController {
         loginSubmitButton.layer.cornerRadius = 5
     }
     
-    func successfulAuthorization(userData: UserData) {
-        let keychain = Keychain(service: "com.rickybooks.rickybooks")
-        keychain["token"] = userData.token
-        keychain["user_id"] = String(userData.user_id)
-        keychain["name"] = userData.name
-        
-        var viewControllers = self.tabBarController?.viewControllers
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        let messagesVC = storyboard.instantiateViewController(withIdentifier: "Messages")
-        messagesVC.tabBarItem = UITabBarItem(title: "Messages", image: UIImage(named: "Messages"), tag: 2)
-        viewControllers![2] = messagesVC
-        
-        let sellVC = storyboard.instantiateViewController(withIdentifier: "Sell")
-        sellVC.tabBarItem = UITabBarItem(title: "Sell", image: UIImage(named: "Sell"), tag: 3)
-        viewControllers![3] = sellVC
-        
-        let profileVC = storyboard.instantiateViewController(withIdentifier: "Profile")
-        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(named: "Profile"), tag: 4)
-        viewControllers![4] = profileVC
-        
-        self.tabBarController?.setViewControllers(viewControllers, animated: false)
-    }
-    
     @objc private func dismissKeyboard(_ sender: Any) {
         view.endEditing(true)
     }
