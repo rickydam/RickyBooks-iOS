7e058056e69310e7f48ddd2ae75344763bb75d86
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-27 18:19:58 -0400

    Refactor login in the AuthorizeVC

--- RickyBooks/AuthorizeViewController.swift
+++ RickyBooks/AuthorizeViewController.swift
@@ -165,56 +165,7 @@ class AuthorizeViewController: UIViewController {
         let emailInput = loginEmailField.text!
         let passwordInput = loginPasswordField.text!
 
-        let endpoint = "https://rickybooks.herokuapp.com/login"
-        var request = URLRequest(url: URL(string: endpoint)!)
-        request.httpMethod = "POST"
-        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        let loginDictionary: [String: Any] = [
-            "email": emailInput,
-            "password": passwordInput
-        ]
-        guard let userObj = try? JSONSerialization.data(withJSONObject: loginDictionary, options: []) else {
-            print("Error with userObj JSONSerialization")
-            return
-        }
-        request.httpBody = userObj
-
-        URLSession.shared.dataTask(with: request) {(data, response, error) in
-            guard let data = data else {
-                print("Error with the data received")
-                return
-            }
-            do {
-                let statusCode = (response as? HTTPURLResponse)?.statusCode
-                if(statusCode == 200) {
-                    let userData = try JSONDecoder().decode(UserData.self, from: data)
-                    DispatchQueue.main.async {
-                        self.successfulAuthorization(userData: userData)
-                    }
-                }
-                else if(statusCode == 401) {
-                    let alert = UIAlertController(title: "Typo! Try again!", message: "Invalid email/password combination", preferredStyle: UIAlertControllerStyle.alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
-                        action in switch action.style {
-                        case .default:
-                            print("Default")
-                        case .cancel:
-                            print("Cancel")
-                        case .destructive:
-                            print("Destructive")
-                        }
-                    }))
-                    DispatchQueue.main.async {
-                        self.present(alert, animated: true, completion: nil)
-                    }
-                }
-                else {
-                    print("Error with the response.")
-                }
-            } catch let jsonError {
-                print("Error with JSONDecoder", jsonError)
-            }
-        }.resume()
+        login(tabBarController: self.tabBarController!, emailInput: emailInput, passwordInput: passwordInput)
     }
     
     override func viewDidLoad() {
