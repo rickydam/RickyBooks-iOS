82e048fd00e44ca87ea9c27a18b585286f2abcbb
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-28 18:26:20 -0400

    Create a Swift file for getBaseUrl func

---
+++ RickyBooks/Functions/GetBaseUrl.swift
@@ -0,0 +1,11 @@
+//
+//  GetBaseUrl.swift
+//  RickyBooks
+//
+//  Created by Ricky Dam on 2018-08-28.
+//  Copyright © 2018 RickyBooks. All rights reserved.
+//
+
+func getBaseUrl() -> String {
+    return "https://rickybooks.herokuapp.com"
+}
